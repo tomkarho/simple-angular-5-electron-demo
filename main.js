d37447c93154e876746f7a8ec052cd9ea5435921
tomkarho <tommi@tomkarho.com>
2018-03-09 11:20:28 +0200

    Electron support

---
+++ main.js
@@ -0,0 +1,35 @@
+// Electron support
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 640,
+    height: 480
+  });
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  win.on('closed', () => win = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
